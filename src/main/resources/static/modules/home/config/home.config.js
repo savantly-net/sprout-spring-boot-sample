d24e1ac8cea5103e30ed87a0e9061f2368434e28
Jeremy <jdbranham@hotmail.com>
2017-09-07 17:24:10 -0500

    added main method

--- src/main/resources/static/modules/home/config/home.config.js
+++ src/main/resources/static/modules/home/config/home.config.js
@@ -1,1 +1,30 @@
-angular.module('home').run([function(){}]);
+angular.module('home').run(['Menus', function(Menus){
+	
+	// Adding items to the main menu 
+	Menus.addMenuItem({
+		menuItemId: 'mainMenuItem',
+		title: 'Main Menu Item', 
+		location: '/'
+	});
+	
+	
+	// Adding items to a new Menu
+	var anotherMenu = Menus.addMenuItem({
+		menuItemId: 'ExampleMenu',
+		title: 'Example Menu', 
+		menuItemType: 'dropdown',
+		roles: ['ADMIN']
+	});
+	anotherMenu.addMenuItem({
+		menuItemId: 'ExampleMenuItem',
+		title: 'Example Item', 
+		location: '/', 
+	});
+	anotherMenu.addMenuItem({
+		menuItemId: 'AnotherExample',
+		title: 'Another', 
+		callback: function(){
+			alert('You clicked it');
+		}
+	});
+}]);
