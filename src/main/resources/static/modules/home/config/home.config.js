31b5f04ff40364bc0958c09eed5aed615a1f6710
Jeremy <jdbranham@hotmail.com>
2017-09-10 11:27:17 -0500

    added props

--- src/main/resources/static/modules/home/config/home.config.js
+++ src/main/resources/static/modules/home/config/home.config.js
@@ -6,25 +6,41 @@ angular.module('home').run(['Menus', function(Menus){
 		title: 'Main Menu Item', 
 		location: '/'
 	});
+	Menus.addMenuItem({
+		menuItemId: 'LogMenuItem',
+		title: 'Log event', 
+		menuItemType: 'callback',
+		callback: function($event){
+			console.log($event);
+		}
+	});
 	
 	
 	// Adding items to a new Menu
-	var anotherMenu = Menus.addMenuItem({
+	Menus.addMenu({menuId: 'anotherMenu', title: 'Another Menu'});
+	
+	Menus.addMenuItem({
+		menuId: 'anotherMenu',
+		 menuItemId: 'exportFileMenuItem',
+		 title: 'Export',
+		 menuItemType: 'callback', 
+		 callback: function($event){console.log($event);}, 
+		 isPublic: false, 
+		 roles: ['user'], 
+		 position: 1
+	});
+	
+	Menus.addMenuItem({
+		menuId: 'anotherMenu',
 		menuItemId: 'ExampleMenu',
 		title: 'Example Menu', 
 		menuItemType: 'dropdown',
 		roles: ['ADMIN']
 	});
-	anotherMenu.addMenuItem({
+	Menus.addMenuItem({
+		menuId: 'anotherMenu',
 		menuItemId: 'ExampleMenuItem',
 		title: 'Example Item', 
 		location: '/', 
-	});
-	anotherMenu.addMenuItem({
-		menuItemId: 'AnotherExample',
-		title: 'Another', 
-		callback: function(){
-			alert('You clicked it');
-		}
 	});
 }]);
